dfac70cbc17ed11cbb2919636d7bc15e6196e6cc
Anastasia Johnson <31184508+anajohnson@users.noreply.github.com>
2020-04-21 11:02:26 +0700

    Add files via upload

---
+++ megacash/cnfg.php
@@ -0,0 +1,12 @@
+<?php
+
+/*Configs By Anonymous24 && Hack to win*/
+
+
+// Add your email //
+$info = 'https://megacash.me/server/backend/login-register/Login.php?email=tran36633@gmail.com&password=pkuoqtkao123..&lang=en';
+
+?>
+
+
+
